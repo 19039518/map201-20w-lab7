a7db335f5406edd22df5fca627c7abe9c04f6ad8
defaultprofile <19039518@g200005-19f.local>
2020-03-05 10:30:54 -0500

    lab7 is done

---
+++ Line.swift
@@ -0,0 +1,14 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Sandeep Kaur on 2020-03-05.
+//  Copyright © 2020 sandeep kaur. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
